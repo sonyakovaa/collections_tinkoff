fe26db7738e61a629cc075edc056213af7924518
Sofya Kovaleva <s.kovaleva@profsoft.pro>
2022-03-22 21:38:32 +0400

    Добавлено решение задания 9.3

--- src/main/java/ru/tinkoff/fintech/MapHandler.java
+++ src/main/java/ru/tinkoff/fintech/MapHandler.java
@@ -1,9 +1,22 @@
 package ru.tinkoff.fintech;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class MapHandler {
     public static Map<String, String> handleMap(Map<String, String> map){
-        return null;
+        Map<String, String> numbers_user = new HashMap<>();
+
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            if (!numbers_user.containsKey(item.getValue())) {
+                numbers_user.put(item.getValue(), item.getKey());
+            }
+            else {
+                String res = numbers_user.get(item.getValue()) + ',' + item.getKey();
+                numbers_user.put(item.getValue(), res);
+            }
+        }
+
+        return numbers_user;
     }
 }
